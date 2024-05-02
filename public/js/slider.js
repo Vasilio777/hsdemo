c51a37a4982ae4679314c86d1d33b51816ecc111
v.kotunov <vasilio_sochi@mail.ru>
2024-05-02 20:59:56 +0700

    update EarthView to read Model data

--- public/js/slider.js
+++ public/js/slider.js
@@ -2,43 +2,46 @@ let isDown = false;
 let startY;
 let scrollTop;
 let totalDuration = 0;
+let eons = $('.eons');
 
 $(document).ready(function() {
-    $('.list-group').on('mousedown', function(e) {
-        isDown = true;
-        $(this).addClass('active');
-        startY = e.pageY - $(this).offset().top;;
-        scrollTop = $(this).scrollTop();
-    });
-    $('.list-group').on('mouseleave', function() {
-        isDown = false;
-        $(this).removeClass('active');
-    });
-    $('.list-group').on('mouseup', function() {
-        isDown = false;
-        $(this).removeClass('active');
-    });
-    $('.list-group').on('mousemove', function(e) {
-        if (!isDown) return;
-        e.preventDefault();
-        const y = e.pageY - $(this).offset().top;
-        const walk = (y - startY) * 3;
-        $(this).scrollTop(scrollTop - walk);
-    });
+    let totaScrolllWidth = $('#scroll-carriage').parent().width();
+
+    // $('.list-group').on('mousedown', function(e) {
+    //     isDown = true;
+    //     $(this).addClass('active');
+    //     startY = e.pageY - $(this).offset().top;;
+    //     scrollTop = $(this).scrollTop();
+    // });
+    // $('.list-group').on('mouseleave', function() {
+    //     isDown = false;
+    //     $(this).removeClass('active');
+    // });
+    // $('.list-group').on('mouseup', function() {
+    //     isDown = false;
+    //     $(this).removeClass('active');
+    // });
+    // $('.list-group').on('mousemove', function(e) {
+    //     if (!isDown) return;
+    //     e.preventDefault();
+    //     const y = e.pageY - $(this).offset().top;
+    //     const walk = (y - startY) * 3;
+    //     $(this).scrollTop(scrollTop - walk);
+    // });
     
-    $('.nav-tabs a').on('click', function(e) {
-        e.preventDefault();
-        $(this).tab('show');
+    // $('.nav-tabs a').on('click', function(e) {
+    //     e.preventDefault();
+    //     $(this).tab('show');
 
-        var classes = $(this).attr('class').split(/\s+/);
-        var eonClass = classes.find(className => className.startsWith('eon-'));
+    //     var classes = $(this).attr('class').split(/\s+/);
+    //     var eonClass = classes.find(className => className.startsWith('eon-'));
 
-        if (eonClass) {
-            let list_header = $('.list-header'); 
-            list_header.removeClass();
-            list_header.addClass('list-header ' + eonClass);
-        }
-    });
+    //     if (eonClass) {
+    //         let list_header = $('.list-header'); 
+    //         list_header.removeClass();
+    //         list_header.addClass('list-header ' + eonClass);
+    //     }
+    // });
 
     
     $('.horizontal-scroll-item').each(function() {
@@ -65,43 +68,55 @@ $(document).ready(function() {
         axis: "x",
         containment: "parent",
         drag: function(event, ui) {
-            let totalWidth = $(this).parent().width();
             let carriageX = ui.position.left;
-            updateScrollText(carriageX / totalWidth);
+            carriage_relative = clamp(carriageX / totaScrolllWidth*1.01, 0, 1.000039); // =)
+            updateScrollText(carriage_relative);
+            updateEonsVisibility(carriage_relative);
         }
     });
 
-    $('.entry').click(function() {
-        let scrollPos = parseFloat($(this).attr('scroll_pos'));
-        let totalWidth = $('.scroller-container').width();
-        let carriageWidth = $('#scroll-carriage').outerWidth();
-        let newLeft = (scrollPos / totalDuration) * totalWidth - (carriageWidth / 2);
-        newLeft = Math.max(0, Math.min(newLeft, totalWidth - carriageWidth));
+    // $('.entry').click(function() {
+    //     let scrollPos = parseFloat($(this).attr('scroll_pos'));
+    //     let carriageWidth = $('#scroll-carriage').outerWidth();
+    //     let newLeft = (scrollPos / totalDuration) * totaScrolllWidth - (carriageWidth / 2);
+    //     newLeft = Math.max(0, Math.min(newLeft, totaScrolllWidth - carriageWidth));
 
-        $('#scroll-carriage').off('click').css('pointer-events', 'none');
-        $('#scroll-carriage').animate({
-            left: newLeft
-        }, {
-            duration: 500,
-            step: function(now, fx) {
-                updateScrollText(now / totalWidth);
-            },
-            complete: function() {
-                $(this).on('click', function() {
-                }).css('pointer-events', 'auto');
+    //     $('#scroll-carriage').off('click').css('pointer-events', 'none');
+    //     $('#scroll-carriage').animate({
+    //         left: newLeft
+    //     }, {
+    //         duration: 500,
+    //         step: function(now, fx) {
+    //             updateScrollText(now / totaScrolllWidth);
+    //         },
+    //         complete: function() {
+    //             $(this).on('click', function() {
+    //             }).css('pointer-events', 'auto');
+    //         }
+    //     });
+    // });
+
+    function updateEonsVisibility(carriageX) {
+        $('.eons').each(function() {
+            let base = parseFloat($(this).attr('base'));
+            let base_end = parseFloat($(this).attr('base_end'));
+            let eonLeft = (base / totalDuration);
+            let eonRight = (base_end / totalDuration);
+            // console.log(carriageX, eonLeft, eonRight);
+            if (eonLeft <= carriageX & eonRight > carriageX) {
+                $(this).show();
+            } else {
+                $(this).hide();
             }
         });
-    });
+    }
 
-    const $lis = $('.list-group .entry');
-    const $randomEntry = $lis.eq(Math.floor(Math.random() * (125 - 105 + 1) + 105));
-    const $tabPane = $randomEntry.closest('.tab-pane');
-    const tabPaneId = $tabPane.attr('id');
-    const $navLink = $('.nav-link[href="#' + tabPaneId + '"]');
-    
-    $navLink.click();
-    $randomEntry.click();
+    function clamp(value, min, max) {
+        return Math.min(Math.max(value, min), max);
+    }
+
     $('body').css('opacity', '1');
+    updateEonsVisibility(0);
 });
 
 function updateScrollText(scrollPos) {
--- resources/views/components/earth_slider.blade.php
+++ resources/views/components/earth_slider.blade.php
@@ -1,49 +1,53 @@
-<div>
-    <div id="nav-container">
-        <div class="nav nav-tabs" id="nav-tab" role="tablist">
-            @foreach ($data as $groupKey => $items)
-                <a class="nav-item nav-link eon-{{ $groupKey}}" id="nav-{{ $groupKey }}-tab" data-toggle="tab" href="#nav-{{ $groupKey }}" role="tab" aria-controls="nav-{{ $groupKey }}" aria-selected="true">{{ $groupKey }}</a>
+<div class="scroller-container">
+    <ul class="horizontal-scroll">
+        @foreach ($data as $groupKey => $items)
+            @foreach ($items as $row)
+                @if(isset($row->eon) && isset($row->duration))
+                    <li class="horizontal-scroll-item" data-eon="{{ $row->eon }}" duration="{{ $row->duration }}">|</li>
+                @endif
             @endforeach
+        @endforeach
+    </ul>
+    <div id="scroll-carriage">
+        <div id="info-block">
+            <span id="info-text">0</span>
+            <span>m.y.a.</span>
         </div>
-        <div class="tab-content" id="nav-tabContent">
-            <div class="list-header">
-                <strong>Age</strong> / <strong>Duration (mln years)</strong>
-            </div>
-            @foreach ($data as $groupKey => $items)
-                <div class="tab-pane fade" id="nav-{{ $groupKey }}" role="tabpanel" aria-labelledby="nav-{{ $groupKey }}-tab">
-                    <ul class="list-group ul-bg">
-                        @foreach ($items as $row)
-                            <li class="entry list-group-item" data-age="{{ $row[4] }}_{{ $row[5] }}" scroll_pos="{{ $row[9] }}">
-                                <span>{{ $row[5] }} </span>
-                                <span>{{ $loop->parent->last ? '~4529,2' : $row[7] }} </span> 
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endforeach
-        </div>
+        <div id="carriage-img"></div>
     </div>
-
-    <div class="scroller-container">
-        <ul class="horizontal-scroll">
-            @foreach ($data as $groupKey => $items)
-                @foreach ($items as $row)
-                    <li class="horizontal-scroll-item" data-eon="{{ $row[0] }}" duration="{{ $row[8] }}">|</li>
-                @endforeach
-            @endforeach
-        </ul>
-        <div id="scroll-carriage">
-            <div id="info-block">
-                <span id="info-text">0</span>
-                <span>m.y.a.</span>
-            </div>
-            <div id="carriage-img"></div>
-        </div>
-        <div class="time-labels">
-            <span class="tl-start">nowadays</span> 
-            <span class="tl-end">past</span>
-        </div>
+    <div class="time-labels">
+        <span class="tl-start">nowadays</span> 
+        <span class="tl-end">past</span>
     </div>
 </div>
 
+<div class="container-fluid">
+    @foreach($data as $key => $eons)
+        <div class="row h-100 eons" base="{{ $eons[0]->base }}" base_end="{{ $eons[0]->base_end }}">
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col">
+                        <div>
+                            <h2>{{ $data[$key][0]->getName() }}</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <div>
+                            <img src="../img/illustration-lock.jpg" alt="Placeholder Image" style="max-width:100%;height:auto;">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="image-container vertical-center">
+                    <img src="{{ $data[$key][0]->getUrl() }}" alt="{{ $data[$key][0]->getName() }}" style="max-width:100%;height:auto;">
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
 
