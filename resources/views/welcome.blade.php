8f46a2f2ec2d3f606253883a2c8be54108afb1b3
v.kotunov <vasilio_sochi@mail.ru>
2024-05-03 03:01:06 +0700

    fill welcome page a bit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,20 @@
 <x-site-layout title="Welcome to the AtmoEcho!">
 
-<span>ola main page</span>
+<h3>Earth's Geological Journey</span>
 
+<h4>
+    Explore the Dynamic History of Our Planet
+</h4>
+
+<div class="row">
+    <div class="col-sm-6">
+Dive into the sweeping narrative of Earth's geological timeline with our interactive exploration module.
+From the vibrant and life-filled era of the Phanerozoic to the ancient origins of the Precambrian,
+nour guide covers each major eon in captivating detail.
+        </div>
+  </div>
+
+<div>
+
+</div>
 </x-site-layout>
